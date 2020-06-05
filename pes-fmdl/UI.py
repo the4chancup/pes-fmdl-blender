c60d0b224ee724cd15ab69ce126404aa8c371c1f
fg4cc <>
2020-06-05 06:49:17 +0200

    Restructured UI logic

--- pes-fmdl/UI.py
+++ pes-fmdl/UI.py
@@ -1,7 +1,188 @@
 import bpy
 import bpy.props
+import bpy_extras.io_utils
 
-from . import Ftex, Operators
+from . import FmdlFile, Ftex, IO
+
+class FMDL_Scene_Import(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
+	"""Load a PES FMDL file"""
+	bl_idname = "import_scene.fmdl"
+	bl_label = "Import Fmdl"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	import_label = "PES FMDL (.fmdl)"
+	
+	filename_ext = ".fmdl"
+	filter_glob = bpy.props.StringProperty(default="*.fmdl", options={'HIDDEN'})
+	
+	def execute(self, context):
+		filename = self.filepath
+		
+		fmdlFile = FmdlFile.FmdlFile()
+		fmdlFile.readFile(filename)
+		
+		IO.importFmdl(context, fmdlFile, filename)
+		
+		return {'FINISHED'}
+
+class FMDL_Scene_Export_Scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
+	"""Export the entire scene as a single PES FMDL file"""
+	bl_idname = "export_scene.fmdl"
+	bl_label = "Export Fmdl"
+	bl_options = {'REGISTER'}
+	
+	export_label = "PES FMDL (.fmdl)"
+	
+	filename_ext = ".fmdl"
+	filter_glob = bpy.props.StringProperty(default="*.fmdl", options={'HIDDEN'})
+	
+	def execute(self, context):
+		fmdlFile = IO.exportFmdl(context, None)
+		fmdlFile.writeFile(self.filepath)
+		
+		self.report({'INFO'}, "Fmdl exported successfully.") 
+		
+		return {'FINISHED'}
+
+class FMDL_Scene_Export_Object(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
+	"""Export an individual object as a PES FMDL file"""
+	bl_idname = "export_scene.fmdl_object"
+	bl_label = "Export Fmdl"
+	bl_options = {'REGISTER'}
+	
+	objectName = bpy.props.StringProperty("Object to export")
+	
+	export_label = "PES FMDL (.fmdl)"
+	
+	filename_ext = ".fmdl"
+	filter_glob = bpy.props.StringProperty(default="*.fmdl", options={'HIDDEN'})
+	
+	@classmethod
+	def poll(cls, context):
+		return context.active_object != None
+	
+	def invoke(self, context, event):
+		self.objectName = context.active_object.name
+		return bpy_extras.io_utils.ExportHelper.invoke(self, context, event)
+	
+	def execute(self, context):
+		fmdlFile = IO.exportFmdl(context, self.objectName)
+		fmdlFile.writeFile(self.filepath)
+		
+		self.report({'INFO'}, "Fmdl exported successfully.") 
+		
+		return {'FINISHED'}
+
+class FMDL_Scene_Panel_FMDL_Compose(bpy.types.Operator):
+	"""Enable separate exporting of the active object"""
+	bl_idname = "fmdl.compose_exportable"
+	bl_label = "Compose Fmdl"
+	bl_options = {'UNDO', 'INTERNAL'}
+	
+	@classmethod
+	def poll(cls, context):
+		return context.active_object != None and not context.active_object.fmdl_file
+	
+	def execute(self, context):
+		context.active_object.fmdl_file = True
+		context.active_object.fmdl_filename = ""
+		return {'FINISHED'}
+
+class FMDL_Scene_Panel_FMDL_Remove(bpy.types.Operator):
+	"""Disable separate exporting"""
+	bl_idname = "fmdl.remove_exportable"
+	bl_label = "Remove"
+	bl_options = {'UNDO', 'INTERNAL'}
+	
+	objectName = bpy.props.StringProperty(name = "Object to remove")
+	
+	def execute(self, context):
+		context.scene.objects[self.objectName].fmdl_file = False
+		return {'FINISHED'}
+
+class FMDL_Scene_Panel_FMDL_Export(bpy.types.Operator):
+	"""Export as PES FMDL file"""
+	bl_idname = "fmdl.export_listed_object"
+	bl_label = "Export Fmdl"
+	bl_options = {'INTERNAL'}
+	
+	objectName = bpy.props.StringProperty(name = "Object to export")
+	
+	def execute(self, context):
+		return bpy.ops.export_scene.fmdl_object(
+			context.copy(),
+			objectName = self.objectName,
+			filepath = context.scene.objects[self.objectName].fmdl_filename
+		)
+
+class FMDL_Scene_Panel_FMDL_Select_Filename(bpy.types.Operator):
+	"""Select a filename to export this FMDL file"""
+	bl_idname = "fmdl.exportable_select_filename"
+	bl_label = "Select Filename"
+	bl_options = {'UNDO', 'INTERNAL'}
+	
+	objectName = bpy.props.StringProperty(name = "Object to export")
+	filepath = bpy.props.StringProperty(subtype = 'FILE_PATH')
+	check_existing = bpy.props.BoolProperty(default = True)
+	filter_glob = bpy.props.StringProperty(default = "*.fmdl")
+	
+	def invoke(self, context, event):
+		context.window_manager.fileselect_add(self)
+		return {'RUNNING_MODAL'}
+	
+	def check(self, context):
+		return True
+	
+	def execute(self, context):
+		context.scene.objects[self.objectName].fmdl_filename = self.filepath
+		return {'FINISHED'}
+
+def FMDL_Scene_FMDL_Import_MenuItem(self, context):
+	self.layout.operator(FMDL_Scene_Import.bl_idname, text=FMDL_Scene_Import.import_label)
+
+def FMDL_Scene_FMDL_Export_MenuItem(self, context):
+	self.layout.operator(FMDL_Scene_Export_Scene.bl_idname, text=FMDL_Scene_Export_Scene.export_label)
+
+class FMDL_Scene_Panel(bpy.types.Panel):
+	bl_label = "FMDL I/O"
+	bl_space_type = "PROPERTIES"
+	bl_region_type = "WINDOW"
+	bl_context = "scene"
+	
+	@classmethod
+	def poll(cls, context):
+		return context.scene != None
+	
+	def draw(self, context):
+		scene = context.scene
+		
+		fmdlFileObjects = []
+		for object in context.scene.objects:
+			if object.fmdl_file:
+				fmdlFileObjects.append(object)
+		fmdlFileObjects.sort(key = lambda object: object.name)
+		
+		mainColumn = self.layout.column()
+		mainColumn.operator(FMDL_Scene_Import.bl_idname)
+		mainColumn.operator(FMDL_Scene_Panel_FMDL_Compose.bl_idname)
+		for object in fmdlFileObjects:
+			box = mainColumn.box()
+			column = box.column()
+			
+			row1 = column.row()
+			row1.label("Object: %s" % object.name)
+			row1.operator(FMDL_Scene_Panel_FMDL_Remove.bl_idname, text = "", icon = 'X').objectName = object.name
+			
+			row2 = column.row(align = True)
+			row2.prop(object, 'fmdl_filename', text = "Export Path")
+			row2.operator(FMDL_Scene_Panel_FMDL_Select_Filename.bl_idname, text = "", icon = 'FILESEL').objectName = object.name
+			
+			row3 = column.row()
+			row3.operator(FMDL_Scene_Panel_FMDL_Export.bl_idname).objectName = object.name
+			if object.fmdl_filename == "":
+				row3.enabled = False
+
+
 
 class FMDL_Mesh_Panel(bpy.types.Panel):
 	bl_label = "FMDL Mesh Settings"
@@ -20,7 +201,9 @@ class FMDL_Mesh_Panel(bpy.types.Panel):
 		mainColumn.prop(mesh, "fmdl_shadow_enum")
 		mainColumn.prop(mesh, "fmdl_alpha_enum")
 
-class Material_Parameter_List_add(bpy.types.Operator):
+
+
+class FMDL_Material_Parameter_List_Add(bpy.types.Operator):
 	bl_idname = "fmdl.material_parameter_add"
 	bl_label = "Add Parameter"
 	
@@ -36,7 +219,7 @@ class Material_Parameter_List_add(bpy.types.Operator):
 		material.fmdl_material_parameter_active = len(material.fmdl_material_parameters) - 1
 		return {'FINISHED'}
 
-class Material_Parameter_List_remove(bpy.types.Operator):
+class FMDL_Material_Parameter_List_Remove(bpy.types.Operator):
 	bl_idname = "fmdl.material_parameter_remove"
 	bl_label = "Remove Parameter"
 	
@@ -53,7 +236,7 @@ class Material_Parameter_List_remove(bpy.types.Operator):
 			material.fmdl_material_parameter_active = len(material.fmdl_material_parameters) - 1
 		return {'FINISHED'}
 
-class Material_Parameter_List_moveup(bpy.types.Operator):
+class FMDL_Material_Parameter_List_MoveUp(bpy.types.Operator):
 	bl_idname = "fmdl.material_parameter_moveup"
 	bl_label = "Move Parameter Up"
 	
@@ -72,7 +255,7 @@ class Material_Parameter_List_moveup(bpy.types.Operator):
 		material.fmdl_material_parameter_active -= 1
 		return {'FINISHED'}
 
-class Material_Parameter_List_movedown(bpy.types.Operator):
+class FMDL_Material_Parameter_List_MoveDown(bpy.types.Operator):
 	bl_idname = "fmdl.material_parameter_movedown"
 	bl_label = "Move Parameter Down"
 	
@@ -91,7 +274,7 @@ class Material_Parameter_List_movedown(bpy.types.Operator):
 		material.fmdl_material_parameter_active += 1
 		return {'FINISHED'}
 
-class Material_Parameter_Name_List(bpy.types.UIList):
+class FMDL_Material_Parameter_Name_List(bpy.types.UIList):
 	def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
 		row = layout.row(align = True)
 		row.alignment = 'EXPAND'
@@ -118,7 +301,7 @@ class FMDL_Material_Panel(bpy.types.Panel):
 		mainColumn.label("Material Parameters")
 		parameterListRow = mainColumn.row()
 		parameterListRow.template_list(
-			Material_Parameter_Name_List.__name__,
+			FMDL_Material_Parameter_Name_List.__name__,
 			"FMDL_Material_Parameter_Name_List",
 			material,
 			"fmdl_material_parameters",
@@ -141,7 +324,9 @@ class FMDL_Material_Panel(bpy.types.Panel):
 				"parameters"
 			)
 
-class Texture_Panel_Load_Ftex(bpy.types.Operator):
+
+
+class FMDL_Texture_Load_Ftex(bpy.types.Operator):
 	"""Load the FTEX texture"""
 	bl_idname = "fmdl.load_ftex"
 	bl_label = "Load FTEX texture"
@@ -163,6 +348,9 @@ class Texture_Panel_Load_Ftex(bpy.types.Operator):
 		Ftex.blenderImageLoadFtex(context.texture.image, bpy.app.tempdir)
 		return {'FINISHED'}
 
+def FMDL_Texture_Load_Ftex_Button(self, context):
+	self.layout.operator(FMDL_Texture_Load_Ftex.bl_idname)
+
 class FMDL_Texture_Panel(bpy.types.Panel):
 	bl_label = "FMDL Texture Settings"
 	bl_space_type = "PROPERTIES"
@@ -181,148 +369,47 @@ class FMDL_Texture_Panel(bpy.types.Panel):
 		mainColumn.prop(texture, "fmdl_texture_filename", text = "Filename")
 		mainColumn.prop(texture, "fmdl_texture_directory", text = "Directory")
 
-class FMDL_Scene_Panel_FMDL_Compose(bpy.types.Operator):
-	"""Enable separate exporting of the active object"""
-	bl_idname = "fmdl.compose_exportable"
-	bl_label = "Compose Fmdl"
-	bl_options = {'UNDO'}
-	
-	@classmethod
-	def poll(cls, context):
-		return context.active_object != None and not context.active_object.fmdl_file
-	
-	def execute(self, context):
-		context.active_object.fmdl_file = True
-		context.active_object.fmdl_filename = ""
-		return {'FINISHED'}
-
-class FMDL_Scene_Panel_FMDL_Remove(bpy.types.Operator):
-	"""Disable separate exporting"""
-	bl_idname = "fmdl.remove_exportable"
-	bl_label = "Remove"
-	bl_options = {'UNDO'}
-	
-	objectName = bpy.props.StringProperty(name = "Object to export")
-	
-	def execute(self, context):
-		context.scene.objects[self.objectName].fmdl_file = False
-		return {'FINISHED'}
-
-class FMDL_Scene_Panel_FMDL_Export(bpy.types.Operator):
-	"""Export as PES FMDL file"""
-	bl_idname = "fmdl.export_listed_object"
-	bl_label = "Export Fmdl"
-	
-	objectName = bpy.props.StringProperty(name = "Object to export")
-	
-	def execute(self, context):
-		return bpy.ops.export_scene.fmdl_object(
-			context.copy(),
-			objectName = self.objectName,
-			filepath = context.scene.objects[self.objectName].fmdl_filename
-		)
-
-class FMDL_Scene_Panel_FMDL_Select_Filename(bpy.types.Operator):
-	"""Select a filename to export this FMDL file"""
-	bl_idname = "fmdl.select_filename"
-	bl_label = "Select Filename"
-	bl_options = {'UNDO'}
-	
-	objectName = bpy.props.StringProperty(name = "Object to export")
-	filepath = bpy.props.StringProperty(subtype = 'FILE_PATH')
-	check_existing = bpy.props.BoolProperty(default = True)
-	filter_glob = bpy.props.StringProperty(default = "*.fmdl")
-	
-	def invoke(self, context, event):
-		context.window_manager.fileselect_add(self)
-		return {'RUNNING_MODAL'}
-	
-	def check(self, context):
-		return True
-	
-	def execute(self, context):
-		context.scene.objects[self.objectName].fmdl_filename = self.filepath
-		return {'FINISHED'}
-	
-	
-
-class FMDL_Scene_Panel(bpy.types.Panel):
-	bl_label = "FMDL I/O"
-	bl_space_type = "PROPERTIES"
-	bl_region_type = "WINDOW"
-	bl_context = "scene"
-	
-	@classmethod
-	def poll(cls, context):
-		return context.scene != None
-	
-	def draw(self, context):
-		scene = context.scene
-		
-		fmdlFileObjects = []
-		for object in context.scene.objects:
-			if object.fmdl_file:
-				fmdlFileObjects.append(object)
-		fmdlFileObjects.sort(key = lambda object: object.name)
-		
-		mainColumn = self.layout.column()
-		mainColumn.operator(Operators.ImportFmdl.bl_idname)
-		mainColumn.operator(FMDL_Scene_Panel_FMDL_Compose.bl_idname)
-		for object in fmdlFileObjects:
-			box = mainColumn.box()
-			column = box.column()
-			
-			row1 = column.row()
-			row1.label("Object: %s" % object.name)
-			row1.operator(FMDL_Scene_Panel_FMDL_Remove.bl_idname, text = "", icon = 'X').objectName = object.name
-			
-			row2 = column.row(align = True)
-			row2.prop(object, 'fmdl_filename', text = "Export Path")
-			row2.operator(FMDL_Scene_Panel_FMDL_Select_Filename.bl_idname, text = "", icon = 'FILESEL').objectName = object.name
-			
-			row3 = column.row()
-			row3.operator(FMDL_Scene_Panel_FMDL_Export.bl_idname).objectName = object.name
-			if object.fmdl_filename == "":
-				row3.enabled = False
-
-def FMDL_Import_MenuItem(self, context):
-	self.layout.operator(Operators.ImportFmdl.bl_idname, text=Operators.ImportFmdl.import_label)
-
-def FMDL_Export_MenuItem(self, context):
-	self.layout.operator(Operators.ExportSceneFmdl.bl_idname, text=Operators.ExportSceneFmdl.export_label)
-
-def FMDL_Load_Ftex_Button(self, context):
-	self.layout.operator(Texture_Panel_Load_Ftex.bl_idname)
-
 
 
 classes = [
-	FMDL_Mesh_Panel,
-	Material_Parameter_List_add,
-	Material_Parameter_List_remove,
-	Material_Parameter_List_moveup,
-	Material_Parameter_List_movedown,
-	Material_Parameter_Name_List,
-	FMDL_Material_Panel,
-	Texture_Panel_Load_Ftex,
-	FMDL_Texture_Panel,
+	FMDL_Scene_Import,
+	FMDL_Scene_Export_Scene,
+	FMDL_Scene_Export_Object,
 	FMDL_Scene_Panel_FMDL_Compose,
 	FMDL_Scene_Panel_FMDL_Remove,
 	FMDL_Scene_Panel_FMDL_Export,
 	FMDL_Scene_Panel_FMDL_Select_Filename,
 	FMDL_Scene_Panel,
+	
+	FMDL_Mesh_Panel,
+	
+	FMDL_Material_Parameter_List_Add,
+	FMDL_Material_Parameter_List_Remove,
+	FMDL_Material_Parameter_List_MoveUp,
+	FMDL_Material_Parameter_List_MoveDown,
+	FMDL_Material_Parameter_Name_List,
+	FMDL_Material_Panel,
+	
+	FMDL_Texture_Load_Ftex,
+	FMDL_Texture_Panel,
 ]
 
 def register():
+	bpy.types.Object.fmdl_file = bpy.props.BoolProperty(name = "Is FMDL file", options = {'SKIP_SAVE'})
+	bpy.types.Object.fmdl_filename = bpy.props.StringProperty(name = "FMDL filename", options = {'SKIP_SAVE'})
+	bpy.types.Material.fmdl_material_parameter_active = bpy.props.IntProperty(name = "FMDL_Material_Parameter_Name_List index", default = -1, options = {'SKIP_SAVE'})
+	
 	for c in classes:
 		bpy.utils.register_class(c)
-	bpy.types.INFO_MT_file_import.append(FMDL_Import_MenuItem)
-	bpy.types.INFO_MT_file_export.append(FMDL_Export_MenuItem)
-	bpy.types.TEXTURE_PT_image.append(FMDL_Load_Ftex_Button)
+	
+	bpy.types.INFO_MT_file_import.append(FMDL_Scene_FMDL_Import_MenuItem)
+	bpy.types.INFO_MT_file_export.append(FMDL_Scene_FMDL_Export_MenuItem)
+	bpy.types.TEXTURE_PT_image.append(FMDL_Texture_Load_Ftex_Button)
 
 def unregister():
-	bpy.types.TEXTURE_PT_image.remove(FMDL_Load_Ftex_Button)
-	bpy.types.INFO_MT_file_export.remove(FMDL_Export_MenuItem)
-	bpy.types.INFO_MT_file_import.remove(FMDL_Import_MenuItem)
+	bpy.types.TEXTURE_PT_image.remove(FMDL_Texture_Load_Ftex_Button)
+	bpy.types.INFO_MT_file_export.remove(FMDL_Scene_FMDL_Export_MenuItem)
+	bpy.types.INFO_MT_file_import.remove(FMDL_Scene_FMDL_Import_MenuItem)
+	
 	for c in classes[::-1]:
 		bpy.utils.unregister_class(c)
--- pes-fmdl/__init__.py
+++ pes-fmdl/__init__.py
@@ -9,35 +9,29 @@ bl_info = {
 import bpy
 import bpy.props
 
-from . import Operators, UI
+from . import UI
 
-class MaterialParameter(bpy.types.PropertyGroup):
+class FMDL_MaterialParameter(bpy.types.PropertyGroup):
 	name = bpy.props.StringProperty(name = "Parameter Name")
 	parameters = bpy.props.FloatVectorProperty(name = "Parameter Values", size = 4, default = [0.0, 0.0, 0.0, 0.0])
 
 def register():
-	bpy.utils.register_class(MaterialParameter)
+	bpy.utils.register_class(FMDL_MaterialParameter)
 	
 	bpy.types.Mesh.fmdl_alpha_enum = bpy.props.IntProperty(name = "Alpha Enum", default = 0, min = 0, max = 255)
 	bpy.types.Mesh.fmdl_shadow_enum = bpy.props.IntProperty(name = "Shadow Enum", default = 0, min = 0, max = 255)
 	
 	bpy.types.Material.fmdl_material_shader = bpy.props.StringProperty(name = "Shader")
 	bpy.types.Material.fmdl_material_technique = bpy.props.StringProperty(name = "Technique")
-	bpy.types.Material.fmdl_material_parameters = bpy.props.CollectionProperty(name = "Material Parameters", type = MaterialParameter)
-	bpy.types.Material.fmdl_material_parameter_active = bpy.props.IntProperty(name = "FMDL_Material_Parameter_Name_List index", default = -1)
+	bpy.types.Material.fmdl_material_parameters = bpy.props.CollectionProperty(name = "Material Parameters", type = FMDL_MaterialParameter)
 	
 	bpy.types.Texture.fmdl_texture_filename = bpy.props.StringProperty(name = "Texture Filename")
 	bpy.types.Texture.fmdl_texture_directory = bpy.props.StringProperty(name = "Texture Directory")
 	bpy.types.Texture.fmdl_texture_role = bpy.props.StringProperty(name = "Texture Role")
 	
-	bpy.types.Object.fmdl_file = bpy.props.BoolProperty(name = "Is FMDL file")
-	bpy.types.Object.fmdl_filename = bpy.props.StringProperty(name = "FMDL filename")
-	
-	Operators.register()
 	UI.register()
 
 def unregister():
 	UI.unregister()
-	Operators.unregister()
 	
 	bpy.utils.unregister_class(MaterialParameter)
